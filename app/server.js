ee8a3896734bc7a8efa6b33b5f1a7bda91085e0a
thecloud lab9 <thecloudlab9@outlook.com>
2025-05-24 15:19:02 +0530

    Initial commit: Node.js Docker app

---
+++ app/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from ECS CI/CD Pipeline!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+
